8557fadce7348d0257cb4ca9fd0ea2f4f06c157e
Wendy Ng <ngwen213@gmail.com>
2017-10-14 15:34:11 -0400

    Add authorization header to request

--- app/src/main/java/com/example/leoniereif/flywithme/delegate/DeltaApiDelegate.java
+++ app/src/main/java/com/example/leoniereif/flywithme/delegate/DeltaApiDelegate.java
@@ -3,6 +3,7 @@ package com.example.leoniereif.flywithme.delegate;
 import android.content.Context;
 import android.util.Log;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -12,6 +13,9 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by sam on 10/14/17.
@@ -40,11 +44,19 @@ public class DeltaApiDelegate {
                     public void onErrorResponse(VolleyError error) {
                         Log.d("Error.Response", error.toString());
                     }
-                }
-        );
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                String credentials = "username:password";
+                String auth = headers.put("Authorization", "Bearer O68OqqGKNrb5EC2hEGE6YIVFGeae");
+                return headers;
+            }
+        };
 
         return response;
     }
+
 
     public String getArrivalTimeByUID(String uID) {
         queue.add(getRequest("http://deltaairlines-dev.apigee.net/v1/hack/flight/status"));
