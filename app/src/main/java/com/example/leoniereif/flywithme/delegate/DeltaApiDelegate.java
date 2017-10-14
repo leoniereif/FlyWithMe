34800df2f177ced4562e839b3cacc44badfff5d3
Wendy Ng <ngwen213@gmail.com>
2017-10-14 18:30:40 -0400

    JSON parse for Delta data from API

--- app/src/main/java/com/example/leoniereif/flywithme/delegate/DeltaApiDelegate.java
+++ app/src/main/java/com/example/leoniereif/flywithme/delegate/DeltaApiDelegate.java
@@ -12,6 +12,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
@@ -27,7 +28,7 @@ public class DeltaApiDelegate {
         queue = Volley.newRequestQueue(context);
     }
 
-    public JsonObjectRequest getRequest(String url) {
+    /*public JsonObjectRequest getRequest(String url) {
         // Request a string response from the provided URL.
         JsonObjectRequest response = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>()
@@ -55,18 +56,61 @@ public class DeltaApiDelegate {
         };
 
         return response;
-    }
+    }*/
 
-    public String getStartAirportByUID(String uID) { return null; }
+    public String getStartAirportByFlightID(String uID) { return null; }
 
-    public String getDestinationAirportByUID(String uID) { return null; }
+    public String getDestinationAirportByFlightID(String uID) { return null; }
 
-    public String getFlightNumberByUID(String uID) { return null; }
+    public String getFlightNumberByUID(String FlightID) { return null; }
 
-    public String getArrivalTimeByUID(String uID) {
-        String uri =
-                String.format("http://deltaairlines-dev.apigee.net/v1/hack/flight/status?flightNumber=1969&flightOriginDate=2017-10-14");
-        queue.add(getRequest(uri));
+    public String getArrivalTimeByFlightID(String flightID, String date) {
+        String url = String.format("http://deltaairlines-dev.apigee.net/v1/hack/flight/status?flightNumber=%s&flightOriginDate=%s", flightID, date);
+        JsonObjectRequest response = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // display response
+                        Log.d("Response", "Response : " + response.toString());
+
+                        try {
+                            JSONObject reader = new JSONObject(response.toString());
+                            JSONObject statusResponse =
+                                    reader.getJSONObject("flightStatusResponse").getJSONObject("statusResponse").getJSONObject("flightStatusTO").getJSONObject("flightStatusLegTOList");
+                            String arrivalLocalTimeActual = statusResponse.getString("arrivalLocalTimeEstimatedActual");
+                            System.out.println(arrivalLocalTimeActual.substring(12,17));
+                                /*
+                                flightStatusResponse
+                                .statusResponse
+                                .flightStatusTO
+                                .flightStatusLegTOList[]
+                                .arrivalLocalTimeEstimatedActual*/
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", "Error : " + error.toString());
+                    }
+                }) {
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer O68OqqGKNrb5EC2hEGE6YIVFGeae");
+                return headers;
+            }
+        };
+
+        queue.add(response);
+        
         return null;
     }
 
