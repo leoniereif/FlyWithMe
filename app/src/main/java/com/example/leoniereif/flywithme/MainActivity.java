0577240fb80debbcdb71ad1c95243303617d7425
Wendy Ng <ngwen213@gmail.com>
2017-10-14 13:11:50 -0400

    Initial Practice Code

--- app/src/main/java/com/example/leoniereif/flywithme/MainActivity.java
+++ app/src/main/java/com/example/leoniereif/flywithme/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.leoniereif.flywithme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView changeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        changeText= (TextView) findViewById(R.id.changeText_tv);
     }
+
+    public void ClickMeOnClick (View view) {
+        changeText.setText("You did it!");
+    }
+
+
 }
