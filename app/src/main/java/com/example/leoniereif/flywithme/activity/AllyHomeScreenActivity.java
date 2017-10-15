42dd20d27c8e3bd79babb610cb63a0fd3d2a2364
leoniereif <lreif3@gatech.edu>
2017-10-15 05:18:59 -0400

    added dummy data in activity

--- app/src/main/java/com/example/leoniereif/flywithme/activity/AllyHomeScreenActivity.java
+++ app/src/main/java/com/example/leoniereif/flywithme/activity/AllyHomeScreenActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.leoniereif.flywithme.R;
 
@@ -14,49 +15,64 @@ import com.example.leoniereif.flywithme.R;
 
 public class AllyHomeScreenActivity extends Activity {
 
-    int currentState = 1;
+    int currentState = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
 
-        ImageView mImageView1;
-        mImageView1 = (ImageView) findViewById(R.id.imageView001);
-        ImageView mImageView2;
-        mImageView2 = (ImageView) findViewById(R.id.imageView002);
-        ImageView mImageView3;
-        mImageView3 = (ImageView) findViewById(R.id.imageView003);
-        ImageView mImageView4;
-        mImageView4 = (ImageView) findViewById(R.id.imageView004);
-        ImageView mImageView5;
-        mImageView5 = (ImageView) findViewById(R.id.imageView005);
-        ImageView mImageView6;
-        mImageView6 = (ImageView) findViewById(R.id.imageView006);
+        ImageView mImageView1 = (ImageView) findViewById(R.id.imageView001);
+        ImageView mImageView2 = (ImageView) findViewById(R.id.imageView002);
+        ImageView mImageView3 = (ImageView) findViewById(R.id.imageView003);
+        ImageView mImageView4 = (ImageView) findViewById(R.id.imageView004);
+        ImageView mImageView5 = (ImageView) findViewById(R.id.imageView005);
+        ImageView mImageView6 = (ImageView) findViewById(R.id.imageView006);
 
+        TextView mTextViewArrivalAP = (TextView) findViewById(R.id.textViewArrivalAP);
+        TextView mTextViewDepartureAP = (TextView) findViewById(R.id.textViewDepartureAP);
+        TextView mTextViewArrivalDate = (TextView) findViewById(R.id.textViewArrivalDate);
+        TextView mTextViewDepartureDate = (TextView) findViewById(R.id.textViewDepartureDate);
 
-        if (currentState == 2) {
+        mTextViewArrivalAP.setText("ATL");
+        mTextViewArrivalDate.setText("01-02-2018");
+        mTextViewDepartureAP.setText("DUS");
+        mTextViewDepartureDate.setText("01-01-2018");
+
+        TextView mTextViewDeparting = (TextView) findViewById(R.id.textViewDeparting);
+        TextView mTextViewLanding = (TextView) findViewById(R.id.textViewLanding);
+        TextView mTextViewBoarding = (TextView) findViewById(R.id.textViewBoarding);
+
+        mTextViewBoarding.setText("12:00PM");
+        mTextViewDeparting.setText("12:30PM");
+        mTextViewLanding.setText("2:00AM");
+
+        if (currentState > 1) {
             mImageView1.setImageResource(R.drawable.checkmark_simple);
             mImageView2.setImageResource(R.drawable.arrow_simple);
 
-        } else if(currentState == 3) {
-            mImageView1.setImageResource(R.drawable.security_blue);
-
-        } else if(currentState == 4) {
-            mImageView1.setImageResource(R.drawable.security_blue);
-
-        } else if(currentState == 5) {
-            mImageView1.setImageResource(R.drawable.security_blue);
-
-        } else if(currentState == 6) {
-            mImageView1.setImageResource(R.drawable.security_blue);
         }
+        if (currentState > 2) {
+            mImageView2.setImageResource(R.drawable.checkmark_simple);
+            mImageView3.setImageResource(R.drawable.arrow_simple);
 
-
+        }
+        if (currentState > 3) {
+            mImageView3.setImageResource(R.drawable.checkmark_simple);
+            mImageView4.setImageResource(R.drawable.arrow_simple);
+        }
+        if (currentState > 4) {
+            mImageView4.setImageResource(R.drawable.checkmark_simple);
+            mImageView5.setImageResource(R.drawable.arrow_simple);
+        }
+        if (currentState > 5) {
+            mImageView5.setImageResource(R.drawable.checkmark_simple);
+            mImageView6.setImageResource(R.drawable.arrow_simple);
+        }
 
     }
 
-    boolean inflated = false;
+    /*boolean inflated = false;
     public void onClickCheckin(View view) {
 
        LinearLayout wholeLl = (LinearLayout) findViewById(R.id.checkin_ll);
@@ -134,7 +150,7 @@ public class AllyHomeScreenActivity extends Activity {
             View pieceLl = findViewById(R.id.parent_new_security_ll);
             wholeLl.removeView(pieceLl);
         }
-    }
+    }*/
 }
 
 
