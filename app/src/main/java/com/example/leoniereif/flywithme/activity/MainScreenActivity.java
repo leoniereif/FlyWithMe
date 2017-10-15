c0e267a0a5d12286e0ed9179feaeab3308f89467
rscsam <sam.costley@yahoo.com>
2017-10-14 21:41:23 -0400

    Added expanding feature to all checkboxes

--- app/src/main/java/com/example/leoniereif/flywithme/activity/MainScreenActivity.java
+++ app/src/main/java/com/example/leoniereif/flywithme/activity/MainScreenActivity.java
@@ -23,10 +23,75 @@ public class MainScreenActivity extends Activity {
 
         LinearLayout wholeLl = (LinearLayout) findViewById(R.id.checkin_ll);
         if (cb.isChecked()) {
-            View pieceLl = LayoutInflater.from(this).inflate(R.layout.checkin_layout, null);
+            View pieceLl = LayoutInflater.from(this).inflate(R.layout.parent_checkin_piece, null);
             wholeLl.addView(pieceLl);
         } else {
-            View pieceLl = findViewById(R.id.new_checkin_ll);
+            View pieceLl = findViewById(R.id.parent_new_checkin_ll);
+            wholeLl.removeView(pieceLl);
+        }
+    }
+
+    public void onClickBaggage(View view) {
+        CheckBox cb = (CheckBox) view;
+
+        LinearLayout wholeLl = (LinearLayout) findViewById(R.id.baggage_ll);
+        if (cb.isChecked()) {
+            View pieceLl = LayoutInflater.from(this).inflate(R.layout.parent_baggage_piece, null);
+            wholeLl.addView(pieceLl);
+        } else {
+            View pieceLl = findViewById(R.id.parent_new_baggage_ll);
+            wholeLl.removeView(pieceLl);
+        }
+    }
+
+    public void onClickInFlight(View view) {
+        CheckBox cb = (CheckBox) view;
+
+        LinearLayout wholeLl = (LinearLayout) findViewById(R.id.in_flight_ll);
+        if (cb.isChecked()) {
+            View pieceLl = LayoutInflater.from(this).inflate(R.layout.parent_inflight_piece, null);
+            wholeLl.addView(pieceLl);
+        } else {
+            View pieceLl = findViewById(R.id.parent_new_inflight_ll);
+            wholeLl.removeView(pieceLl);
+        }
+    }
+
+    public void onClickBoarding(View view) {
+        CheckBox cb = (CheckBox) view;
+
+        LinearLayout wholeLl = (LinearLayout) findViewById(R.id.boarding_ll);
+        if (cb.isChecked()) {
+            View pieceLl = LayoutInflater.from(this).inflate(R.layout.parent_boarding_piece, null);
+            wholeLl.addView(pieceLl);
+        } else {
+            View pieceLl = findViewById(R.id.parent_new_boarding_ll);
+            wholeLl.removeView(pieceLl);
+        }
+    }
+
+    public void onClickGate(View view) {
+        CheckBox cb = (CheckBox) view;
+
+        LinearLayout wholeLl = (LinearLayout) findViewById(R.id.gate_ll);
+        if (cb.isChecked()) {
+            View pieceLl = LayoutInflater.from(this).inflate(R.layout.parent_gate_piece, null);
+            wholeLl.addView(pieceLl);
+        } else {
+            View pieceLl = findViewById(R.id.parent_new_gate_ll);
+            wholeLl.removeView(pieceLl);
+        }
+    }
+
+    public void onClickSecurity(View view) {
+        CheckBox cb = (CheckBox) view;
+
+        LinearLayout wholeLl = (LinearLayout) findViewById(R.id.security_ll);
+        if (cb.isChecked()) {
+            View pieceLl = LayoutInflater.from(this).inflate(R.layout.parent_security_piece, null);
+            wholeLl.addView(pieceLl);
+        } else {
+            View pieceLl = findViewById(R.id.parent_new_security_ll);
             wholeLl.removeView(pieceLl);
         }
     }
