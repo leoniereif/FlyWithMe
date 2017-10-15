165c0e284d4d7fc0b740bb834ced51c547bf8941
rscsam <sam.costley@yahoo.com>
2017-10-15 05:32:37 -0400

    Merge branches 'generateUID' and 'mainScreenUI' of https://github.com/leoniereif/FlyWithMe into generateUID
    # Conflicts:
    #	app/src/main/java/com/example/leoniereif/flywithme/activity/AllyHomeScreenActivity.java

--- app/src/main/java/com/example/leoniereif/flywithme/activity/GenerateUIDActivity.java
+++ app/src/main/java/com/example/leoniereif/flywithme/activity/GenerateUIDActivity.java
@@ -63,7 +63,7 @@ public class GenerateUIDActivity extends AppCompatActivity {
         String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         StringBuilder salt = new StringBuilder();
         Random rnd = new Random();
-        while (salt.length() < 16) { // length of the random string.
+        while (salt.length() < 8) { // length of the random string.
             int index = (int) (rnd.nextFloat() * chars.length());
             salt.append(chars.charAt(index));
         }
